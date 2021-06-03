fe2b7754e104a7167b56346f6bc19f7b6d02c0b8
jahidhasan6484 <jahidhasan6484@gmail.com>
2021-06-03 20:25:58 +0600

    heading added

--- Components/TopBar/TopBar.js
+++ Components/TopBar/TopBar.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
-import { FontAwesome5, FontAwesome } from '@expo/vector-icons'
+import { FontAwesome5, FontAwesome } from '@expo/vector-icons';
+// import { useFonts, Pacifico_400Regular } from '@expo-google-fonts/pacifico';
 
 const TopBar = () => {
+    // const [fontsLoaded] = useFonts({
+    //     Pacifico_400Regular,
+    // });
+
     return (
         <>
-        <Text>Adda 24</Text>
-        <View style={styles.container}>
-            <FontAwesome5 name="fire" size={20} color="#F06795" />
-            <FontAwesome name="comments" size={20} color="#5c5c5c" />
-            <FontAwesome name="user" size={20} color="#5c5c5c" />
-        </View></>
-        
+            {/* <View>
+                <Text style={styles.heading}>adda24</Text>
+            </View> */}
+            <View style={styles.container}>
+                <FontAwesome5 name="fire" size={20} color="#F06795" />
+                <FontAwesome name="comments" size={20} color="#5c5c5c" />
+                <FontAwesome name="user" size={20} color="#5c5c5c" />
+            </View></>
     );
 };
 
 export default TopBar;
 
 const styles = StyleSheet.create({
+    // heading: {
+    //     fontSize: 24,
+    //     fontFamily: 'Pacifico_400Regular',
+    //     textAlign: 'center',
+    //     fontSize: 20,
+    //     paddingVertical: 5,
+    // },
     container: {
         height: 65,
         flexDirection: 'row',
