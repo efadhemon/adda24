198155eb070aa2e3be9787c3fbe2e577b0154d54
jahidhasan6484 <jahidhasan6484@gmail.com>
2021-06-03 18:41:54 +0600

    topbar added

---
+++ Components/TopBar/TopBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const TopBar = () => {
+    return (
+        <div>
+            <Text>This is TopBar of Adda-24</Text>
+        </div>
+    );
+};
+
+export default TopBar;
